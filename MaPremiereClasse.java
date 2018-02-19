f89749d8b4397e2b5f9e77a3dd454c170e426cad
motak-cfpt <36639680+motak-cfpt@users.noreply.github.com>
2018-02-19 18:04:01 +0100

    Add files via upload

---
+++ MaPremiereClasse.java
@@ -0,0 +1,12 @@
+class MaPremiereClasse 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Voici un nouvel arrivé dans le monde Java!");
+		System.out.println("Je m'appele " + args[0]);
+		double d = 1234.568679;
+		int x = d;
+
+	}
+}
